ac4280192796b508043cfc3b8a420cf3ce4fcf16
gabs <110247388+GabsEdits@users.noreply.github.com>
2022-12-18 08:57:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,50 +6,46 @@ new Vue({
   
     } });
 
-class RingDot {
+
+class CircleAndDot {
   constructor() {
     this.root = document.body
     this.cursor = document.querySelector(".curzr")
-    this.dot = document.querySelector(".curzr-dot")
 
-    this.pointerX = 0
-    this.pointerY = 0
-    this.cursorSize = 15
+    this.position = {
+      distanceX: 0, 
+      distanceY: 0,
+      distance: 0,
+      pointerX: 0,
+      pointerY: 0,
+    },
+    this.previousPointerX = 0
+    this.previousPointerY = 0
+    this.angle = 0
+    this.previousAngle = 0
+    this.angleDisplace = 0
+    this.degrees = 57.296
+    this.cursorSize = 20
+    this.fading = false
 
     this.cursorStyle = {
       boxSizing: 'border-box',
       position: 'fixed',
-      display: 'flex',
       top: `${ this.cursorSize / -2 }px`,
       left: `${ this.cursorSize / -2 }px`,
       zIndex: '2147483647',
-      justifyContent: 'center',
-      alignItems: 'center',
       width: `${ this.cursorSize }px`,
       height: `${ this.cursorSize }px`,
       backgroundColor: '#fff0',
-      boxShadow: '0 0 0 1.25px #111920, 0 0 0 2.25px #EE6C4D',
+      border: '1.25px solid #111920',
       borderRadius: '50%',
-      transition: '200ms, transform 100ms',
+      boxShadow: '0 -15px 0 -8px #0000',
+      transition: '250ms, transform 100ms',
       userSelect: 'none',
       pointerEvents: 'none'
     }
 
-    this.dotStyle = {
-      boxSizing: 'border-box',
-      position: 'fixed',
-      zIndex: '2147483647',
-      width: '4px',
-      height: '4px',
-      backgroundColor: '#111920',
-      boxShadow: '0 0 0 1px #EE6C4D',
-      borderRadius: '50%',
-      userSelect: 'none',
-      pointerEvents: 'none',
-    }
-
     this.init(this.cursor, this.cursorStyle)
-    this.init(this.dot, this.dotStyle)
   }
 
   init(el, style) {
@@ -63,29 +59,74 @@ class RingDot {
   }
 
   move(event) {
+    this.previousPointerX = this.position.pointerX
+    this.previousPointerY = this.position.pointerY
+    this.position.pointerX = event.pageX + this.root.getBoundingClientRect().x
+    this.position.pointerY = event.pageY + this.root.getBoundingClientRect().y
+    this.position.distanceX = this.previousPointerX - this.position.pointerX
+    this.position.distanceY = this.previousPointerY - this.position.pointerY
+    this.distance = Math.sqrt(this.position.distanceY ** 2 + this.position.distanceX ** 2)
+
     if (event.target.localName === 'button' || 
         event.target.localName === 'a' || 
         event.target.onclick !== null ||
         event.target.className.includes('curzr-hover')) {
-      this.hover(40)
+      this.hover()
     } else {
       this.hoverout()
     }
-    
-    this.pointerX = event.pageX + this.root.getBoundingClientRect().x
-    this.pointerY = event.pageY + this.root.getBoundingClientRect().y
-  
-    this.cursor.style.transform = `translate3d(${this.pointerX}px, ${this.pointerY}px, 0)`
+
+    this.cursor.style.transform = `translate3d(${this.position.pointerX}px, ${this.position.pointerY}px, 0)`
+
+    this.rotate(this.position)
+    this.fade(this.distance)
   }
 
-  hover(radius) {
-    this.cursor.style.width = this.cursor.style.height = `${radius}px`
-    this.cursor.style.top = this.cursor.style.left = `${radius / -2}px`
+  rotate(position) {
+    let unsortedAngle = Math.atan(Math.abs(position.distanceY) / Math.abs(position.distanceX)) * this.degrees
+    this.previousAngle = this.angle
+
+    if (position.distanceX <= 0 && position.distanceY >= 0) {
+      this.angle = 90 - unsortedAngle + 0
+    } else if (position.distanceX < 0 && position.distanceY < 0) {
+      this.angle = unsortedAngle + 90
+    } else if (position.distanceX >= 0 && position.distanceY <= 0) {
+      this.angle = 90 - unsortedAngle + 180
+    } else if (position.distanceX > 0 && position.distanceY > 0) {
+      this.angle = unsortedAngle + 270
+    }
+
+    if (isNaN(this.angle)) {
+      this.angle = this.previousAngle
+    } else {
+      if (this.angle - this.previousAngle <= -270) {
+        this.angleDisplace += 360 + this.angle - this.previousAngle
+      } else if (this.angle - this.previousAngle >= 270) {
+        this.angleDisplace += this.angle - this.previousAngle - 360
+      } else {
+        this.angleDisplace += this.angle - this.previousAngle
+      }
+    }
+    this.cursor.style.transform += ` rotate(${this.angleDisplace}deg)`
+  }
+
+  hover() {
+    this.cursor.style.border = '10px solid #111920'
   }
 
   hoverout() {
-    this.cursor.style.width = this.cursor.style.height = `${this.cursorSize}px`
-    this.cursor.style.top = this.cursor.style.left = `${this.cursorSize / -2}px`
+    this.cursor.style.border = '1.25px solid #111920'
+  }
+
+  fade(distance) {
+    this.cursor.style.boxShadow = `0 ${-15 - distance}px 0 -8px #111920, 0 0 0 1px #F2F5F8`
+    if (!this.fading) {
+      this.fading = true
+      setTimeout(() => {
+        this.cursor.style.boxShadow = '0 -15px 0 -8px #11192000, 0 0 0 1px #F2F5F8'
+        this.fading = false
+      }, 50)
+    }
   }
 
   click() {
@@ -97,12 +138,11 @@ class RingDot {
 
   remove() {
     this.cursor.remove()
-    this.dot.remove()
   }
 }
 
 (() => {
-  const cursor = new RingDot()
+  const cursor = new CircleAndDot()
   if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     document.onmousemove = function (event) {
       cursor.move(event)
